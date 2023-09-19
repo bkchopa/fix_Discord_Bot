8c20452d635e294c76dbc68a7bc9381c00c98a6f
Lee SeonBeom <bkchopa@nexon.co.kr>
2023-09-19 16:35:14 +0900

    복구6 7시 리셋 부활

--- main.py
+++ main.py
@@ -87,8 +87,7 @@ async def on_ready():
 
 
     await ch.send("내전 봇 재시작(약 24시간마다 자동재시작)")
-    #resetList.start()
-
+    resetList.start()
     counter.start()
 
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("내전 명단관리 열심히"))
