21f637b60300123b0c5ffdf8f62f2c7924a7de7d
Lee SeonBeom <bkchopa@nexon.co.kr>
2023-10-13 14:13:30 +0900

    Revert "api 추가"
    This reverts commit b8711f8d04503bffda98d671947774ab795db7ec.

--- web_server.py
+++ web_server.py
@@ -1,33 +1,9 @@
 import os
-from flask import Flask, jsonify
-import json
-import main
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello, World!"
 
-@app.route('/api/greet', methods=['GET'])
-def greet_api():
-    users = [
-        {
-            "id": 1,
-            "name": "John Doe",
-            "email": "john.doe@example.com",
-            "age": 30
-        },
-        {
-            "id": 2,
-            "name": "Jane Smith",
-            "email": "jane.smith@example.com",
-            "age": 28
-        },
-        {
-            "id": 3,
-            "name": "Bob Brown",
-            "email": "bob.brown@example.com",
-            "age": 22
-        }
-    ]
-    return jsonify({"message": "Hello from the API!", "users": users})
