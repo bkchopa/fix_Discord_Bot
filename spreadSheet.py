a247454e561ef8642592be265879279979477a72
Lee SeonBeom <bkchopa@nexon.co.kr>
2023-10-18 16:56:06 +0900

    오류 수정 2

--- spreadSheet.py
+++ spreadSheet.py
@@ -27,8 +27,11 @@ except json.JSONDecodeError:
     SPREADSHEET_IDS = {}  # JSON 파싱에 실패하면 빈 딕셔너리를 사용합니다.
 
 # 워크시트 선택
-last_key = list(SPREADSHEET_IDS.keys())[-1]  # 딕셔너리의 마지막 키 가져오기
-last_spreadsheet_id = SPREADSHEET_IDS[last_key]
+if SPREADSHEET_IDS:
+    last_key = list(SPREADSHEET_IDS.keys())[-1]  # 딕셔너리의 마지막 키 가져오기
+    last_spreadsheet_id = SPREADSHEET_IDS[last_key]
+else:
+    last_spreadsheet_id = None  # SPREADSHEET_IDS가 빈 딕셔너리인 경우에 대비
 rankingSheet = client.open_by_key(last_spreadsheet_id).worksheet("내전 순위")
 # 데이터 읽기와 쓰기 예제
 #print(worksheet.get_all_records())  # 모든 데이터 가져오기
